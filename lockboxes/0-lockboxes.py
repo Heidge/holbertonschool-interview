630ddb5f47e8e92a4f3f9c0f5968b22f4e12d7df
Heidge <axel.gore35@gmail.com>
2023-08-25 17:54:16 +0200

    push task algo 2 files

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    new_list = []
+    i = 0
+    j = 0
+    x = 0
+    for i in boxes:
+            new_list.append(boxes[i][x])
+            if i > 0:
+                while new_list != null:
+                    if new_list[j] == boxes[i]:
+                        break
+                    else:
+                        break
+                        return (false)
+
+
+
