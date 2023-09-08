4dd162d281f31c8c64a65d83e77cb72c2c001125
Heidge <axel.gore35@gmail.com>
2023-09-08 17:49:19 +0200

    push algo task

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,19 +3,21 @@
 """function for determine the fewest operations needed"""
 
 def minOperations(n):
+
+   
    l = 1
    phrase = 0
    count = 0
-
-   if (n < 1):
-      return 0
+    
+    if (n < 1):
+       return 0
    
-   while (l != n):
-      if (n % l == 0):
-         phrase = l
-         count += 1
-      l += phrase
-      count += 1
+    while (l != n):
+       if (n % l == 0):
+          phrase = l
+          count += 1
+       l += phrase
+       count += 1
 
-   return count
+    return count
   
