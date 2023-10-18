3155bd8a1921499013b127872b206e36001f4179
Heidge <axel.gore@outlook.fr>
2023-10-18 13:23:45 +0200

    test

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -2,3 +2,27 @@
 """
 Function for utf8 validation
 """
+
+def validUTF8(data):
+    num_bytes = 0
+    
+    for num in data:
+        if num >= 128:  # Les caractères UTF-8 commencent toujours par 0b0 ou 0b11
+            if num_bytes == 0:
+                if (num >> 5) == 0b110:
+                    num_bytes = 1
+                elif (num >> 4) == 0b1110:
+                    num_bytes = 2
+                elif (num >> 3) == 0b11110:
+                    num_bytes = 3
+                else:
+                    return False
+            else:
+                if (num >> 6) != 0b10:
+                    return False
+                num_bytes -= 1
+        else:
+            if num_bytes != 0:
+                return False
+    
+    return True
