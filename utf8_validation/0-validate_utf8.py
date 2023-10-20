fc32763e1931d9d4dd9324787251676363dbe60d
Heidge <axel.gore@outlook.fr>
2023-10-20 11:20:33 +0200

    push new interview task

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,34 +6,45 @@ Function for utf8 validation
 
 def validUTF8(data):
 
-	dataL = len(data)
 	binary_data = []
-	count = 0
-	i = 1
-	
+	index = 0
+
 	for num in data:
 		binary_string = bin(num)
 		binary_data.append(binary_string[2:])
 
 	print(binary_data)
-	if binary_data[0][:5] == "11111":
-		return False
+	print(binary_data[0][1:4])
 
-	first_string = str(binary_data[0][:4])
-
-	for char in first_string:
-		if char == '1':
-			count += 1
-	
 	for binary_num in binary_data:
-		if i == count:
-			break
-		else:
-			if binary_num[i][:2] == "10" or binary_num[i][:1] == "0":
-				continue
-				i += 1
-			else:
+			if check_bit(binary_num) or check_size(binary_num):
 				return False
-
+			else:
+				if binary_num[0] == 0:
+					continue
+				else:
+					count = check_after(binary_num)
+					
 	return True
  
+def check_after(binary_num):
+	count = 0
+	for char in binary_num[1:4]:
+		if char == '1':
+			count += 1
+		else:
+			break
+	return count
+
+def check_bit(binary_num):
+	count = 0
+	for char in binary_num:
+		count += 1
+	if count > 8:
+		return True
+	return False
+	#couper si supérieur à 8
+
+def check_size(binary_num):
+	if binary_num[:5] == "11111":
+		return True
