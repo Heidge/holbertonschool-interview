d9e2f45a75ed64bb2d25730a898b2bd8baba8ea4
Axel Goré <axel.gore@outlook.fr>
2023-12-06 16:12:16 +0100

    push starwars api js script

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+function doRequest(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(JSON.parse(body));
+            }
+        });
+    });
+}
+
+async function main() {
+    try {
+        const filmData = await doRequest(url);
+
+        for (const characterURL of filmData.characters) {
+            try {
+                const characterData = await doRequest(characterURL);
+                console.log(characterData.name);
+            } catch (error) {
+                console.error('Erreur:', error);
+            }
+        }
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
+}
+
+main();
