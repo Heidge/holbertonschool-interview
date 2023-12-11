67a90b6b019cebb9e7dfd217094f2563a0c8a78a
Axel Goré <axel.gore@outlook.fr>
2023-12-11 17:09:07 +0100

    fix documentation

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+"""
+Calcule la quantite d'eau retenue entre des murs representes par un tableau.
+
+Parameters:
+walls (list): Un tableau representant la hauteur des murs.
+
+Returns:
+int: La quantite totale d'eau retenue entre les murs.
+"""
 
 def rain(walls):
-    """
-    Calcule la quantité d'eau retenue entre des murs représentés par un tableau.
-
-    Parameters:
-    walls (list): Un tableau représentant la hauteur des murs.
-
-    Returns:
-    int: La quantité totale d'eau retenue entre les murs.
-    """
 
     # Initialisation des variables
     index = 0
@@ -17,31 +17,31 @@ def rain(walls):
     nbBorne = 0
     wallHeight = 0
 
-    # Si le tableau est vide, aucune eau ne peut être retenue
-    if len(walls) == 0:
+    # Si le tableau est vide, aucune eau ne peut etre retenue
+    if not walls:
         return 0
 
     # Parcours du tableau des murs
     while index < len(walls):
         nbZero = 0
 
-        # Ignorer les zéros aux extrémités du tableau
-        if (index == 0 and walls[0] == 0) or (index == len(walls) - 1 and walls[len(walls) - 1] == 0):
+        # Ignorer les zéros aux extremites du tableau
+        if (index == 0 and walls[0] == 0) or (index == len(walls) - 1 and walls[-1] == 0):
             index += 1
-            pass
+            continue
 
-        # Recherche de séquences de zéros entre les murs
+        # Recherche de sequences de zeros entre les murs
         while index < len(walls):
             if walls[index] == 0:
                 nbZero += 1
                 index += 1
             else:
-                # Enregistrement de la hauteur du mur si c'est la première borne
+                # Enregistrement de la hauteur du mur si c'est la premiere borne
                 if nbBorne == 0:
                     wallHeight = walls[index]
                 nbBorne += 1
 
-                # Si la deuxième borne est trouvée, calcul de la quantité d'eau retenue
+                # Si la deuxieme borne est trouvee, calcul de la quantite d'eau retenue
                 if nbBorne == 2:
                     secondWall = walls[index]
                     if secondWall < wallHeight:
