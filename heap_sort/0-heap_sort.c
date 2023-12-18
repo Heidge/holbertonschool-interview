4b6400556aea388c815d9ab2a665d2241d21199e
Axel Goré <axel.gore@outlook.fr>
2023-12-18 17:15:42 +0100

    fix betty

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,37 +1,36 @@
 #include "sort.h"
 
-
-void swap(int *a, int *b) 
+void swap(int* a, int* b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void sift_down(int *array, size_t root, size_t size, size_t sizes) 
+void sift_down(int* array, size_t root, size_t size, size_t sizes)
 {
     size_t max_child, swap_index;
 
-    while (2 * root + 1 < size) 
+    while (2 * root + 1 < size)
     {
         max_child = 2 * root + 1;
         swap_index = root;
 
-        if (array[swap_index] < array[max_child]) 
+        if (array[swap_index] < array[max_child])
         {
             swap_index = max_child;
         }
 
-        if (max_child + 1 < size && array[swap_index] < array[max_child + 1]) 
+        if (max_child + 1 < size && array[swap_index] < array[max_child + 1])
         {
             swap_index = max_child + 1;
         }
 
-        if (swap_index == root) 
+        if (swap_index == root)
         {
             break;
-        } 
-        else 
+        }
+        else
         {
             swap(&array[root], &array[swap_index]);
             print_array(array, sizes);
@@ -40,24 +39,23 @@ void sift_down(int *array, size_t root, size_t size, size_t sizes)
     }
 }
 
-void heapify(int *array, size_t size) 
+void heapify(int* array, size_t size)
 {
     size_t i;
-    
-    for (i = size / 2; i > 0; i--) 
+
+    for (i = size / 2; i > 0; i--)
     {
         sift_down(array, i - 1, size, size);
     }
 }
 
-void heap_sort(int *array, size_t size) 
+void heap_sort(int* array, size_t size)
 {
-
     size_t i;
 
     heapify(array, size);
-    
-    for (i = size - 1; i > 0; i--) 
+
+    for (i = size - 1; i > 0; i--)
     {
         swap(&array[0], &array[i]);
         print_array(array, size);
