050b8d43cc64b6a57719327217a4c1ff477f63a6
Axel Goré <axel.gore@outlook.fr>
2024-02-26 14:51:10 +0100

    push weekly algo

---
+++ making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Making change function
+"""
+
+def makeChange(coins, total):
+    coins.sort(reverse=True)
+    count = 0
+    length = len(coins)
+    coins_copy = coins.copy()
+
+    if total == 0:
+        return 0
+        
+    for i in range(length):
+        if coins[i] < 0:
+            return -1
+        while (total >= coins[i]):
+            count += total / coins[i]
+            total = total % coins[i]
+    if total != 0:  
+        if length != 0:
+            if length > 1:
+                coins_copy.pop(1)
+                return makeChange(coins_copy, total)
+            else:
+                coins.pop(0)
+                return makeChange(coins, total)
+        else:
+            return -1
+    else:
+        return round(count)
+    
