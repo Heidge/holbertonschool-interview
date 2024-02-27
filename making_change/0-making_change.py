e8bd2282f3719bb3bff421f08833afcb6c897abd
Axel Goré <axel.gore@outlook.fr>
2024-02-27 16:28:07 +0100

    push weekly algo

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -18,7 +18,7 @@ def makeChange(coins, total):
     for i in range(length):
         if coins[i] < 0:
             return -1
-        while (total >= coins[i]):
+        while (total > coins[i]):
             count += total / coins[i]
             total = total % coins[i]
     if total != 0:
