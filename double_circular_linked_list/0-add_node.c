17703c47c57e0ece2b1c72258cdfb01f52ab4dc2
Axel Goré <axel.gore@outlook.fr>
2024-03-06 11:25:01 +0100

    push weekly algo

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/**
+ * add_node_end - Add a new node to the end of a double circular linked list
+ * @list: pointer to the pointer to the head of the list
+ * @str: string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+    List *new_node = malloc(sizeof(List));
+    List *last = *list;
+
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->str = malloc(strlen(str) + 1);
+    if (new_node->str == NULL) {
+        free(new_node);
+        return NULL;
+    }
+
+    strcpy(new_node->str, str);
+
+    if (*list == NULL) {
+        new_node->prev = new_node;
+        new_node->next = new_node;
+        *list = new_node;
+    } else {
+        last = (*list)->prev;
+        last->next = new_node;
+        new_node->prev = last;
+        new_node->next = *list;
+        (*list)->prev = new_node;
+    }
+
+    return new_node;
+}
+
+/**
+ * add_node_begin - Add a new node to the beginning of a double circular linked list
+ * @list: pointer to the pointer to the head of the list
+ * @str: string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+    List *new_node = malloc(sizeof(List));
+
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->str = malloc(strlen(str) + 1);
+    if (new_node->str == NULL) {
+        free(new_node);
+        return NULL;
+    }
+
+    strcpy(new_node->str, str);
+
+    if (*list == NULL) {
+        new_node->prev = new_node;
+        new_node->next = new_node;
+    } else {
+        new_node->prev = (*list)->prev;
+        new_node->next = *list;
+        (*list)->prev->next = new_node;
+        (*list)->prev = new_node;
+    }
+
+    *list = new_node;
+
+    return new_node;
+}
