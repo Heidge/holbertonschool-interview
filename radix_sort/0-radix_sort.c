2a8c3f7d40c084f9acbf5d2aa202beb484ab0d4c
Axel Goré <axel.gore@outlook.fr>
2024-03-11 17:06:09 +0100

    push weekly algo

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -3,20 +3,19 @@
 #include <math.h>
 #include "sort.h"
 
-
-int numLength(int num)
+int numLength(int num) 
 {
     int count = 0;
 
-    while(num > 0)
+    while (num > 0) 
     {
         count++;
-        num = num/10;;
+        num = num / 10;
     }
-    return (count);
+    return count;
 }
 
-int getMax(int *array, int n)
+int getMax(int *array, int n) 
 {
     int i = 0;
     int mx = array[0];
@@ -26,63 +25,70 @@ int getMax(int *array, int n)
     return mx;
 }
 
-double my_pow(double base, int exponent) 
+int my_pow(int base, int exponent) 
 {
-    double result = 1.0;
+    int result = 1.0;
     int i;
     
-    if (exponent < 0) {
+    if (exponent < 0) 
+    {
         base = 1.0 / base;
         exponent = -exponent;
     }
     
-    for (i = 0; i < exponent; i++) {
+    for (i = 0; i < exponent; i++) 
+    {
         result *= base;
     }
     
     return result;
 }
 
-void radix_sort(int *array, size_t size)
-{
-    int i = 0;
-    int j = 0;
-    int k = 0;
+void radix_sort(int *array, size_t size) {
+    int i, j;
     int len = size;
     int duration = numLength(getMax(array, size));
+    
+    int *tmp_array = (int *)malloc(len * sizeof(int));
 
-    for (; j < duration; j++)
-    {
-        if (j == 0)
-        {
-            for (i = 0; i < len; i++)
-            {
-                for(k = 0; k < len; k++)
-                {
-                    if (array[k] % 10 > array[i] % 10)
-                    {
-                        int tmp_array = array[k];
-                        array[k] = array[i];
-                        array[i] = tmp_array;
-                    }
-                }
-            }   
+    if (tmp_array == NULL) {
+        fprintf(stderr, "Erreur d'allocation mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (j = 0; j < duration; j++) {
+        // Copier array dans tmp_array à chaque itération
+        for (i = 0; i < len; i++) {
+            tmp_array[i] = array[i];
         }
-        else
-        {
-            for (i = 0; i < len; i++)
-            {
-                for(k = 0; k < len; k++)
-                {
-                    if (array[k] / my_pow(10, i) > array[i] / my_pow(10, i))
-                    {
-                        int tmp_array = array[k];
-                        array[k] = array[i];
-                        array[i] = tmp_array;
-                    }
-                }
-            }
+
+        int count[10] = {0};
+        int output[len];
+
+        // Compter le nombre d'occurrences de chaque chiffre
+        for (i = 0; i < len; i++) {
+            count[(tmp_array[i] / my_pow(10, j)) % 10]++;
         }
+
+        // Ajuster le tableau count pour contenir les positions réelles
+        for (i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Construire le tableau de sortie
+        for (i = len - 1; i >= 0; i--) {
+            output[count[(tmp_array[i] / my_pow(10, j)) % 10] - 1] = tmp_array[i];
+            count[(tmp_array[i] / my_pow(10, j)) % 10]--;
+        }
+
+        // Copier le tableau de sortie dans le tableau original
+        for (i = 0; i < len; i++) {
+            array[i] = output[i];
+        }
+
         print_array(array, size);
     }
+
+    free(tmp_array);
 }
+
