dbb08453436ece111e0cc36b36b3a68cd819d7fd
Axel Goré <axel.gore@outlook.fr>
2024-03-11 15:18:39 +0100

    push weekly algo

--- radix_sort/sort.h
+++ radix_sort/sort.h
@@ -1,7 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <math.h>
+
 void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
+int numLength(int *num);
+int getMax(int array, int n);
 
 #endif
