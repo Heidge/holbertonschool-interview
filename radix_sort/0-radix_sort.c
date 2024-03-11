ea2ad6f481a2bbd99d54a0eb30a9a34c7daf5d92
Axel Goré <axel.gore@outlook.fr>
2024-03-11 15:15:37 +0100

    push weekly algo

---
+++ radix_sort/0-radix_sort.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "sort.h"
+
+int numLength(int num) 
+{
+    int count = 0;
+
+    while (num > 0) {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+
+int getMax(int *array, int n) 
+{
+    int mx = array[0];
+    for (int i = 1; i < n; i++)
+        if (array[i] > mx)
+            mx = array[i];
+    return mx;
+}
+
+void radix_sort(int *array, size_t size) 
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int len = size;
+    int duration = numLength(getMax(array, size));
+    
+    int *tmp_array = (int *)malloc(len * sizeof(int));
+
+    if (tmp_array == NULL) 
+    {
+        fprintf(stderr, "Erreur d'allocation mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (; j < duration; j++) 
+    {
+        if (j == 0) 
+        {
+            for (i = 0; i < len; i++) {
+                for (k = 0; k < len; k++) {
+                    if (array[k] % 10 > array[i] % 10) {
+                        tmp_array[k] = array[k];
+                        array[k] = array[i];
+                        array[i] = tmp_array[k];
+                    }
+                }
+            }
+        } 
+        else 
+        {
+            for (i = 0; i < len; i++) {
+                for (k = 0; k < len; k++) {
+                    if (array[k] / pow(10, i) > array[i] / pow(10, i)) {
+                        tmp_array[k] = array[k];
+                        array[k] = array[i];
+                        array[i] = tmp_array[k];
+                    }
+                }
+            }
+        }
+        print_array(array, size);
+    }
+
+    free(tmp_array); // Libérer la mémoire allouée pour le tableau temporaire
+}
---
+++ radix_sort/README.md
@@ -0,0 +1,43 @@
+# Radix Sort
+
+## More info
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+- Our file print_array.c will be compiled with your function during the correction.
+- Please declare the prototype of the function print_array in your sort.h header file
+
+## Tasks
+
+### 0. Radix sort
+
+Write a function that sorts an array of integers in ascending order using the Radix sort algorithm
+
+- Prototype: void radix_sort(int *array, size_t size);
+- You must implement the LSD radix sort algorithm
+- You can assume that array will contain only numbers >= 0
+- You are allowed to use malloc and free for this task
+- You’re expected to print the array each time you increase your significant digit (See example below)
+
