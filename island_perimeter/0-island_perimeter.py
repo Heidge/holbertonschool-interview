2bfca050ad6fedeff161d903be17f27ad0b39ba1
Axel Goré <axel.gore@outlook.fr>
2024-03-20 10:42:53 +0100

    push weekly algo

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Method that calculates the perimeter of an island completely surrounded by water
+Method that calculates the perimeter of an island
 """
 
 
 def island_perimeter(grid):
     """
-    Method that calculates the perimeter of an island completely surrounded by water
+    Method that calculates the perimeter of an island
     """
     perimeter = 0
-    
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
@@ -21,5 +21,5 @@ def island_perimeter(grid):
                     perimeter += 1
                 if j == len(grid[0])-1 or grid[i][j+1] == 0:
                     perimeter += 1
-                
+          
     return perimeter
